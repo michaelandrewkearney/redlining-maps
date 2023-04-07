637a504cb9f0397ee6aca85e2b102fec33dbf484
Michael Kearney <michaelkearney55@gmail.com>
2023-04-07 14:27:53 -0400

    feat:reimplement SearchGeoJSONHandler

--- backend/src/main/java/edu/brown/cs32/ezhang29mkearne1/server/SearchGeoJSONHandler.java
+++ backend/src/main/java/edu/brown/cs32/ezhang29mkearne1/server/SearchGeoJSONHandler.java
@@ -7,7 +7,6 @@ import edu.brown.cs32.ezhang29mkearne1.geoData.GeoJSON.Feature;
 import edu.brown.cs32.ezhang29mkearne1.server.errorResponses.BadJsonException;
 import edu.brown.cs32.ezhang29mkearne1.server.errorResponses.BadRequestException;
 import edu.brown.cs32.ezhang29mkearne1.server.errorResponses.ErrorResponse;
-import edu.brown.cs32.ezhang29mkearne1.server.layer.search.CachedSearcher;
 import edu.brown.cs32.ezhang29mkearne1.server.layer.search.ExpensiveSearcher;
 import edu.brown.cs32.ezhang29mkearne1.server.layer.search.Searcher;
 import edu.brown.cs32.ezhang29mkearne1.server.response.ServerResponses;
@@ -15,13 +14,11 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 public final class SearchGeoJSONHandler implements Route {
   private final ServerState state;
-  private Searcher<FeatureCollection, Feature> searcher;
-  private FeatureCollection stashedSearch = new FeatureCollection("FeatureCollection", new ArrayList<>());
+  private Searcher<FeatureCollection, Feature> filterer;
 
   public SearchGeoJSONHandler(ServerState state) {
     this.state = state;
@@ -35,31 +32,31 @@ public final class SearchGeoJSONHandler implements Route {
   }
   @Override
   public Object handle(Request request, Response response) throws Exception {
-//    String query = request.queryParams("query");
-//    if (query != null) {
-//      FeatureCollection results = cachedSearcher.search((Feature f) -> {
-//        f.props().containsKey("area_description_data");
-//        for (String s : ((Map<String, String>) f.props().get("area_description_data")).values()) {
-//          if (s.contains(query)) {
-//            return true;
-//          }
-//        }
-//        return false;
-//      });
-//      stashedSearch = results;
-//      searcher = new ExpensiveSearcher<>(stashedSearch);
-//    }
-//    try {
-//      double minLon = Double.parseDouble(request.queryParams("minLon"));
-//      double minLat = Double.parseDouble(request.queryParams("minLat"));
-//      double maxLon = Double.parseDouble(request.queryParams("maxLon"));
-//      double maxLat = Double.parseDouble(request.queryParams("maxLat"));
-//      BoundingBox box = new BoundingBox(minLon, minLat, maxLon, maxLat);
-//      FeatureCollection results = searcher.search((GeoJSON.Feature feat) -> box.contains(feat));
-//      return new ServerResponses.FeatureCollectionResponse("searchGeoJSON", results).serialize();
-//    } catch (NumberFormatException e) {
-//      return new ErrorResponse(new BadRequestException("BoundingBox params required.", Map.copyOf(request.params()))).serialize();
-//    }
+    String query = request.queryParams("query");
+    if (query != null) {
+      FeatureCollection results = state.getSearcher().search((Feature f) -> {
+        f.props().containsKey("area_description_data");
+        for (String s : ((Map<String, String>) f.props().get("area_description_data")).values()) {
+          if (s.contains(query)) {
+            return true;
+          }
+        }
+        return false;
+      });
+      filterer = new ExpensiveSearcher<>(results);
+    }
+    try {
+      
+      double minLon = Double.parseDouble(request.queryParams("minLon"));
+      double minLat = Double.parseDouble(request.queryParams("minLat"));
+      double maxLon = Double.parseDouble(request.queryParams("maxLon"));
+      double maxLat = Double.parseDouble(request.queryParams("maxLat"));
+      BoundingBox box = new BoundingBox(minLon, minLat, maxLon, maxLat);
+      FeatureCollection results = filterer.search((GeoJSON.Feature feat) -> box.contains(feat));
+      return new ServerResponses.FeatureCollectionResponse("searchGeoJSON", results).serialize();
+    } catch (NumberFormatException e) {
+      return new ErrorResponse(new BadRequestException("BoundingBox params required.", Map.copyOf(request.params()))).serialize();
+    }
     return new ErrorResponse(new BadJsonException("search not implement yet whoops", "json"));
   }
 }
